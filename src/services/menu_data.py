2005375c034d6ccb681bb1fe94027a7d70480f1b
Lucas Matos <lucaslmatos18@gmail.com>
2023-09-01 19:14:18 -0300

    Req 03 - Mapeamento dos Pratos - Ingredientes

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,37 @@
+import csv
+
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.source_path = source_path
+        self.dishes = set()
+        self.load_data()
+
+    def load_data(self) -> None:
+        with open(self.source_path, "r") as file:
+            csv_reader = csv.DictReader(file)
+            unique_dishes = []
+
+            for row in csv_reader:
+                dish_name = row["dish"]
+                dish_price = float(row["price"])
+                ingredient_name = row["ingredient"]
+                recipe_amount = int(row["recipe_amount"])
+
+                for dish in self.dishes:
+                    if dish.name == dish_name:
+                        ingredient = Ingredient(ingredient_name)
+                        dish.add_ingredient_dependency(
+                            ingredient, recipe_amount)
+
+                if dish_name not in unique_dishes:
+                    unique_dishes.append(dish_name)
+                    current_dish = Dish(dish_name, dish_price)
+                    self.dishes.add(current_dish)
+                    ingredient = Ingredient(ingredient_name)
+                    current_dish.add_ingredient_dependency(
+                        ingredient, recipe_amount)
