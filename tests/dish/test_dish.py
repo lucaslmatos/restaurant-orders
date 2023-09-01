7ddf5b241e084e79cfe5bc2d0624a7da938f78dc
Lucas Matos <lucaslmatos18@gmail.com>
2023-09-01 16:00:37 -0300

    Req 02 - Dish tests

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,42 @@
 from src.models.dish import Dish  # noqa: F401, E261, E501
+from typing import Dict
+
+from src.models.ingredient import Ingredient
+import pytest
+
+Recipe = Dict[Ingredient, int]
 
 
 # Req 2
 def test_dish():
-    pass
+    right_dish = Dish("macarronada", 20.00)
+    wrong_dish = Dish("feijoada", 25.00)
+    right_ingredient = Ingredient("queijo mussarela")
+
+    with pytest.raises(
+            TypeError, match="Dish price must be float."):
+        Dish("sandwiche de presunto", "10")
+
+    with pytest.raises(
+            ValueError, match="Dish price must be greater then zero."):
+        Dish("pizza", -56.00)
+
+    assert right_dish.name == "macarronada"
+
+    assert right_dish.__eq__(right_dish) is True
+
+    assert right_dish.__eq__(wrong_dish) is False
+
+    assert right_dish.__hash__() == hash(right_dish.__repr__())
+
+    assert right_dish.__hash__() != hash(wrong_dish.__repr__())
+
+    assert right_dish.__repr__() == "Dish('macarronada', R$20.00)"
+
+    right_dish.add_ingredient_dependency(right_ingredient, 5)
+
+    assert right_dish.recipe == {right_ingredient: 5}
+
+    assert right_dish.get_ingredients() == {right_ingredient}
+
+    assert right_dish.get_restrictions() == right_ingredient.restrictions
